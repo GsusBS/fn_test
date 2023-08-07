ce876cd0f45ece9b1ac04e47b7e941c787d80b7a
GsusBS <GsusBSr@gmail.com>
2023-08-07 19:18:38 +0200

    feat: create features files and app router config

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:fn_test/features/home/presentation/pages/home_page.dart';
+import 'package:go_router/go_router.dart';
+
+enum AppRoute {
+  home,
+}
+
+final GoRouter goRouter = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      name: AppRoute.home.name,
+      builder: (context, state) =>
+          const MyHomePage(title: 'Flutter Demo Home Page'),
+    ),
+  ],
+);
